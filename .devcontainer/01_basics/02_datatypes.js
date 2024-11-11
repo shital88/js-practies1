34a93fa7e1bbef3e212e077ec957d80ef45ed096
shital88 <shitalm08@gmail.com>
2024-11-11 08:16:01 +0000

    datatapes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert(3+3)//we are using nodejs, not browser
+
+let name="shital"
+let age=18
+let isLoggedIn=false
+let state;
+//number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object/
