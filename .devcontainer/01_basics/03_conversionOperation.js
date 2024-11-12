ae1789ac9afb885e93363c27d4870e1cd164fba8
shital88 <shitalm08@gmail.com>
2024-11-12 09:28:19 +0000

    oeration

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score="33as"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
  //"33" => 33
 //"33ads" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 let isLoggedIn=1
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1 => true; 0 => false
 //"" => false
@@ -21,5 +21,9 @@ console.log(booleanIsLoggedIn)
 
 let someNumber=32
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+let value=3
+let negValue=-(value)
+console.log(negValue);
