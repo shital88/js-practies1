4f5e51d97cbf7a46c95ae57ce7e5c3b552f7e9a4
shital88 <shitalm08@gmail.com>
2024-11-24 01:03:17 +0000

    math and nums

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,47 @@
+const name="shital"
+
+const repoCount=50
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('shital-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString=gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString =gameName.slice(-8,5)
+// console.log(anotherString);
+
+// const newStringOne="    shital   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url="https://shital.com/shital%10mane"
+
+// console.log(url.replace('%10','-'));
+
+// console.log(url.includes('shital'));
+
+// console.log(url.includes('jadhav'));
+
+// console.log(gameName.split('-'))
+
+// console.log(gameName.at(7))
+
+//console.log(gameName.bold());
+console.log(gameName.blink());
+
+
+
+
+
+
+
---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+const score=400
+// console.log(score)
+
+const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber=123.8996
+// console.log(typeof(otherNumber.toPrecision(4)));
+
+const hunderds=1000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++++++++   MATHS  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
