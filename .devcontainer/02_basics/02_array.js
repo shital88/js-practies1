6f830bb0ff5a375236e72e05d95bdaa17f01dbab
shital88 <shitalm08@gmail.com>
2024-11-30 01:03:43 +0000

    array

---
+++ .devcontainer/02_basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros=["thor", "Ironman", "spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1]);
+
+// const allHeros=marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+//const allNewArray=[...marvel_heros, ...dc_heros]
+// console.log(allNewArray);
+
+// const newArray=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const realArray=newArray.flat(Infinity)
+
+// console.log((realArray));
+
+console.log(Array.isArray("javascript"));
+console.log(Array.from("javascript"));
+
+
+
+
+
+
