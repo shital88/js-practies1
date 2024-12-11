20bb44cf3e8aed906d4ae1fc52fc0c28c90cf20f
shital88 <shitalm08@gmail.com>
2024-12-11 00:32:41 +0000

    do while

---
+++ .devcontainer/05_iteration/02_do.js
@@ -0,0 +1,19 @@
+let index=0
+while (index<=10) {
+    // console.log(`valeu of index is ${index}`);
+    index=index+2    
+}
+
+let myArray=['flash','batman','superman']
+let arr=0
+
+while (arr<myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr++    
+}
+
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
